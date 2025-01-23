6d9d0abf19fd8b47382e690b785999ea6bf36420
yue <127387510+Full-ofstarsl@users.noreply.github.com>
2025-01-23 10:22:48 +0800

    Add files via upload

---
+++ rabbitmq/rabbitmq.go
@@ -0,0 +1,115 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"github.com/streadway/amqp"
+)
+
+type RabbitMQ struct {
+	channel  *amqp.Channel
+	Name     string
+	exchange string
+}
+
+func New(s string) *RabbitMQ {
+	// 尝试与mq服务器建立连接
+	conn, e := amqp.Dial(s)
+	if e != nil {
+		panic(e)
+	}
+
+	ch, e := conn.Channel() // opne new channel
+	if e != nil {
+		panic(e)
+	}
+	// declare:声明
+	// use a channel to declare a queue
+	q, e := ch.QueueDeclare(
+		"",    // name(队列名未指定,由mq自动生成)
+		false, // durable
+		true,  // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if e != nil {
+		panic(e)
+	}
+
+	mq := new(RabbitMQ)
+	mq.channel = ch
+	mq.Name = q.Name
+	return mq
+}
+
+// 将队列绑定到交换机
+// 生产者 --> 交换机 --> 队列 --> 消费者
+func (q *RabbitMQ) Bind(exchange string) {
+	e := q.channel.QueueBind(
+		q.Name,   // queue name
+		"",       // routing key
+		exchange, // exchange
+		false,
+		nil)
+	if e != nil {
+		panic(e)
+	}
+	q.exchange = exchange
+}
+
+// 交换机向队列推送消息
+func (q *RabbitMQ) Send(queue string, body interface{}) {
+	str, e := json.Marshal(body)
+	if e != nil {
+		panic(e)
+	}
+	e = q.channel.Publish("",
+		queue,
+		false,
+		false,
+		amqp.Publishing{
+			ReplyTo: q.Name,
+			Body:    []byte(str),
+		})
+	if e != nil {
+		panic(e)
+	}
+}
+
+// 生产者向交换机推送消息
+func (q *RabbitMQ) Publish(exchange string, body interface{}) {
+	str, e := json.Marshal(body)
+	if e != nil {
+		panic(e)
+	}
+	e = q.channel.Publish(exchange,
+		"",
+		false,
+		false,
+		amqp.Publishing{
+			ReplyTo: q.Name,
+			Body:    []byte(str),
+		})
+	if e != nil {
+		panic(e)
+	}
+}
+
+// 消费者从队列消费消息
+func (q *RabbitMQ) Consume() <-chan amqp.Delivery {
+	c, e := q.channel.Consume(q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if e != nil {
+		panic(e)
+	}
+	return c
+}
+func (q *RabbitMQ) Close() {
+	q.channel.Close()
+}
